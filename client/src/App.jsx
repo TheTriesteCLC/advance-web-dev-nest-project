0c9a741f64c1419d0eeb40e0b84a54e630c310ed
tbnguyen21vp <tbnguyen21@vp.fitus.edu.vn>
2025-01-07 15:07:43 +0700

    update 7/1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import AdminRouter from "./routes/Admin";
 import EmployeeRouter from "./routes/Employee";
@@ -22,31 +21,13 @@ function App() {
       default:
         return AuthRouter;
     }
-=======
-import  AdminRouter from "./routes/Admin";
-import StaffRouter from "./routes/Staff";
-import CustomerRouter from "./routes/Customer";
-
-import { Fragment, Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Loading from "./components/err/loading";
-const NotfoundError = lazy(() => import("~/components/err"));
-
-function App() {
-  const VerifyRoure = () => {
-    return CustomerRouter;
->>>>>>> b41e90baba50aea756eb95c614ca0422242aaa7b
   };
   return (
     <>
       <Router>
         <Suspense fallback={<Loading />}>
           <Routes>
-<<<<<<< HEAD
             {VerifyRoure().map((route, index) => {
-=======
-            {CustomerRouter.map((route, index) => {
->>>>>>> b41e90baba50aea756eb95c614ca0422242aaa7b
               const Layout = route.Layout === null ? Fragment : route.Layout;
               const Page =
                 route.component === null ? Fragment : route.component;
